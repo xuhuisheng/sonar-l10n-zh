359fd0dd25b610b75cd9a576e733fd03bba3dcd7
unknown <xuhuisheng@B000000011390l.internal.baidu.com>
2012-10-24 23:01:23 +0800

    try to migrate 3.2 to 3.3.

--- src/test/java/org/sonar/plugins/l10n/ChinesePackPluginTest.java
+++ src/test/java/org/sonar/plugins/l10n/ChinesePackPluginTest.java
@@ -21,35 +21,11 @@ package org.sonar.plugins.l10n;
 
 import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.sonar.test.i18n.I18nMatchers.assertAllBundlesUpToDate;
+import org.sonar.test.i18n.I18nMatchers;
 
 public class ChinesePackPluginTest {
-
-  /**
-   * Version of Sonar which is covered by the language pack
-   */
-  private static final String SONAR_VERSION = "3.2";
-
-  /**
-   * Bundles of the forge plugins covered by the language pack
-   */
-
-  private static final Map<String, String> pluginIdsToBundleUrlMap = new HashMap<String, String>() {
-    {
-      // put here the plugins covered by this language pack
-
-      // The "java.properties" core bundle must currently be checked manually
-      // => see http://jira.codehaus.org/browse/SONAR-3735
-      put("java", "https://raw.github.com/SonarSource/sonar/3.2/plugins/sonar-l10n-en-plugin/src/main/resources/org/sonar/l10n/java.properties");
-    }
-  };
-
   @Test
-  public void test() throws Exception {
-    assertAllBundlesUpToDate(SONAR_VERSION, pluginIdsToBundleUrlMap);
+  public void bundles_should_be_up_to_date() {
+    I18nMatchers.assertBundlesUpToDate();
   }
-
 }
