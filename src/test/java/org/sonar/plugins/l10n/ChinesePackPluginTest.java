41349ef780e325865138141050df6d712170137d
lingo <lingo@mossle.com>
2016-02-26 20:38:59 +0800

    prepare to support sonarqube-5.3

--- src/test/java/org/sonar/plugins/l10n/ChinesePackPluginTest.java
+++ src/test/java/org/sonar/plugins/l10n/ChinesePackPluginTest.java
@@ -1,7 +1,7 @@
 /*
  * L10n :: Chinese Pack
- * Copyright (C) 2014 Mossle
- * dev@sonar.codehaus.org
+ * Copyright (C) 2014-2016 SonarSource SA
+ * mailto:contact AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -13,25 +13,28 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 package org.sonar.plugins.l10n;
 
 import org.junit.Test;
 import org.sonar.test.i18n.I18nMatchers;
 
-import static org.fest.assertions.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ChinesePackPluginTest {
+  
+  // https://jira.sonarsource.com/browse/SONAR-7226
   @Test
   public void bundles_should_be_up_to_date() {
     I18nMatchers.assertBundlesUpToDate();
   }
+  
 
   @Test
-  public void should_not_declare_extensions() {
+  public void should_not_have_extensions() {
     assertThat(new ChinesePackPlugin().getExtensions()).isEmpty();
   }
 }
